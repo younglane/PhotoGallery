431eb055f53c5e94181352a34398e2511c870aba
Lane Young <youngla13@students.ecu.edu>
2020-06-17 00:56:25 -0400

    End of Chapter 24

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=b551b5d1661a8398defd41dc2d1d6195" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
